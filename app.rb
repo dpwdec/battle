11928c23c34a7baee8a5dcf414c904f7ea5fc23c
dpwdec <declankolakowski@gmail.com>
2020-04-29 14:32:27 +0100

    Added player name entry form and play url

--- app.rb
+++ app.rb
@@ -2,11 +2,15 @@ require 'sinatra/base'
 
 class Battle < Sinatra::Base
   get '/' do
-    'Testing infrastructure working!'
+    erb(:index)
+  end
+
+  post '/play' do
+    @player_1 = params[:player1]
+    @player_2 = params[:player2]
+    erb(:play)
   end
 
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
-
-
